f4d2db346c2187a3a33ac93b2928123f7cd435ca
Scott Nemes <scottmnemes@gmail.com>
2024-12-03 10:28:54 -0800

    Day3 solution and switched template to using sample.txt to start

--- 2024/day3/computers.go
+++ 2024/day3/computers.go
@@ -4,6 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 type value struct {
@@ -19,17 +22,27 @@ func solution1() int {
 	}
 	defer inputs.Close()
 
+	total := 0
+
 	scanner := bufio.NewScanner(inputs)
 	for scanner.Scan() {
 		line := scanner.Text()
-		_ = line
+		r, _ := regexp.Compile(`mul\((\d+)\,(\d+)\)`)
+		ops := r.FindAllString(line, -1)
+		for _, op := range ops {
+			r2, _ := regexp.Compile(`(\d+)`)
+			nums := r2.FindAllString(op, -1)
+			num1, _ := strconv.Atoi(nums[0])
+			num2, _ := strconv.Atoi(nums[1])
+			total += (num1 * num2)
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
 		fmt.Println("Failed to read from input puzzle inputs, RIP:", err)
 	}
 
-	return 0
+	return total
 }
 
 func solution2() int {
@@ -40,17 +53,38 @@ func solution2() int {
 	}
 	defer inputs.Close()
 
+	total := 0
+
 	scanner := bufio.NewScanner(inputs)
+	enabled := true
 	for scanner.Scan() {
 		line := scanner.Text()
-		_ = line
+		r, _ := regexp.Compile(`mul\((\d+)\,(\d+)\)|do\(\)|don\'t\(\)`)
+		ops := r.FindAllString(line, -1)
+		for _, op := range ops {
+			command := strings.Split(op, "(")
+			if command[0] == "do" {
+				enabled = true
+				continue
+			} else if command[0] == "don't" {
+				enabled = false
+				continue
+			}
+			if enabled {
+				r2, _ := regexp.Compile(`(\d+)`)
+				nums := r2.FindAllString(op, -1)
+				num1, _ := strconv.Atoi(nums[0])
+				num2, _ := strconv.Atoi(nums[1])
+				total += (num1 * num2)
+			}
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
 		fmt.Println("Failed to read from input puzzle inputs, RIP:", err)
 	}
 
-	return 0
+	return total
 }
 
 func main() {
