fcfaaec75a377e1d12c77025d79ab940fa5842ae
Scott Nemes <scottmnemes@gmail.com>
2024-12-04 13:33:22 -0800

    Temporary

--- 2024/day4/crossword.go
+++ 2024/day4/crossword.go
@@ -19,14 +19,44 @@ func solution1() int {
 	}
 	defer inputs.Close()
 
+	dx := 0
+	dy := 0
 	scanner := bufio.NewScanner(inputs)
 	for scanner.Scan() {
+		if dx == 0 {
+			line := scanner.Text()
+			dy = len(line)
+		}
+		dx += 1
+	}
+
+	puzzle := make([][]string, dy)
+	for i := range puzzle {
+		puzzle[i] = make([]string, dx)
+	}
+
+	inputs.Seek(0, 0)
+
+	y := 0
+	scanner = bufio.NewScanner(inputs)
+	for scanner.Scan() {
 		line := scanner.Text()
-		_ = line
+		for x, c := range line {
+			puzzle[y][x] = string(c)
+		}
+		y += 1
 	}
 
 	if err := scanner.Err(); err != nil {
 		fmt.Println("Failed to read from input puzzle inputs, RIP:", err)
+	}
+
+	//words := make(map[string]int)
+
+	for y := 0; y < len(puzzle); y++ {
+		for x := 0; x < len(puzzle[0]); x++ {
+			fmt.Println(x, y)
+		}
 	}
 
 	return 0
