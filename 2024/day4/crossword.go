a4ae311a0a8a1ad5428d5626e889b1d1bd448a8a
Scott Nemes <scottmnemes@gmail.com>
2024-12-04 18:18:05 -0800

    Temporary

--- 2024/day4/crossword.go
+++ 2024/day4/crossword.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 )
 
 type value struct {
@@ -19,47 +20,94 @@ func solution1() int {
 	}
 	defer inputs.Close()
 
-	dx := 0
-	dy := 0
+	puzzle_rows := []string{}
+
 	scanner := bufio.NewScanner(inputs)
 	for scanner.Scan() {
-		if dx == 0 {
-			line := scanner.Text()
-			dy = len(line)
-		}
-		dx += 1
-	}
-
-	puzzle := make([][]string, dy)
-	for i := range puzzle {
-		puzzle[i] = make([]string, dx)
-	}
-
-	inputs.Seek(0, 0)
-
-	y := 0
-	scanner = bufio.NewScanner(inputs)
-	for scanner.Scan() {
 		line := scanner.Text()
-		for x, c := range line {
-			puzzle[y][x] = string(c)
-		}
-		y += 1
+		puzzle_rows = append(puzzle_rows, line)
 	}
 
 	if err := scanner.Err(); err != nil {
 		fmt.Println("Failed to read from input puzzle inputs, RIP:", err)
 	}
 
-	//words := make(map[string]int)
+	puzzle_cols := []string{}
 
-	for y := 0; y < len(puzzle); y++ {
-		for x := 0; x < len(puzzle[0]); x++ {
-			fmt.Println(x, y)
+	for x := 0; x < len(puzzle_rows[0]); x++ {
+		s := ""
+		for y := 0; y < len(puzzle_rows); y++ {
+			s += string(puzzle_rows[y][x])
 		}
+		puzzle_cols = append(puzzle_cols, s)
 	}
 
-	return 0
+	puzzle_rows_reversed := make([]string, len(puzzle_rows))
+	_ = copy(puzzle_rows_reversed, puzzle_rows)
+
+	for i, s := range puzzle_rows_reversed {
+		temp := ""
+		for i := len(s) - 1; i >= 0; i-- {
+			temp += string(s[i])
+		}
+		puzzle_rows_reversed[i] = temp
+	}
+
+	puzzle_diags := []string{}
+
+	for y := len(puzzle_rows) - 1; y >= 0; y-- {
+		s1 := ""
+		s2 := ""
+		for y2, x := y, 0; y2 < len(puzzle_rows) && x <= x; y2, x = y2+1, x+1 {
+			s1 += string(puzzle_rows[y2][x])
+			s2 += string(puzzle_rows_reversed[y2][x])
+		}
+		// puzzle_diags = append(puzzle_diags, s1)
+		// puzzle_diags = append(puzzle_diags, s2)
+	}
+
+	for y := 0; y < len(puzzle_rows); y++ {
+		s1 := ""
+		s2 := ""
+		for y2, x := y, y+1; y2 < len(puzzle_rows) && x < len(puzzle_rows[0]); y2, x = y2+1, x+1 {
+			s1 += string(puzzle_rows[y2][x])
+			s2 += string(puzzle_rows_reversed[y2][x])
+		}
+		puzzle_diags = append(puzzle_diags, s1)
+		// puzzle_diags = append(puzzle_diags, s2)
+	}
+
+	r, _ := regexp.Compile(`XMAS|SAMX`)
+	r2, _ := regexp.Compile(`XMASAMX|SAMXMAS`)
+
+	total := 0
+
+	for _, s := range puzzle_rows {
+		matches := r.FindAllString(s, -1)
+		total += len(matches)
+		matches2 := r2.FindAllString(s, -1)
+		total += len(matches2)
+	}
+
+	for _, s := range puzzle_cols {
+		matches := r.FindAllString(s, -1)
+		total += len(matches)
+		matches2 := r2.FindAllString(s, -1)
+		total += len(matches2)
+	}
+
+	for _, s := range puzzle_diags {
+		matches := r.FindAllString(s, -1)
+		total += len(matches)
+		matches2 := r2.FindAllString(s, -1)
+		total += len(matches2)
+	}
+
+	for _, row := range puzzle_diags {
+		fmt.Println(row)
+	}
+
+	return total
 }
 
 func solution2() int {
